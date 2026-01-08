f380378caa8b4443c3318d0efbaa57431e0668cd
イざけ <akha3743@gmail.com>
2026-01-08 16:59:37 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './public/**/*.{html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
